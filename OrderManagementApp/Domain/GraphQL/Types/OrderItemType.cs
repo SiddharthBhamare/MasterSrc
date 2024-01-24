b7a15f9ac48d163aab11048989633b10cdf3668e
SiddharthBhamare <sidbhamare85@gmail.com>
2024-01-24 19:39:39 +0530

    GraphQL Simple CRUID Ops

---
+++ OrderManagementApp/Domain/GraphQL/Types/OrderItemType.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Models;
+
+namespace Domain.GraphQL.Types
+{
+    public class OrderItemType : ObjectType<OrderItem>
+    {
+        protected override void Configure(IObjectTypeDescriptor<OrderItem> descriptor)
+        {
+            descriptor.BindFieldsExplicitly();
+            descriptor.Field(f => f.OrderItemId).Description("ID");
+            descriptor.Field(f => f.OrderId);
+            descriptor.Field(f => f.Quantity);
+            descriptor.Field(f => f.MenuItemId);
+            descriptor.Field(f => f.Order);
+        }
+    }
+}
---
+++ OrderManagementApp/Domain/RestaurantDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Domain
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public RestaurantDbContext(DbContextOptions dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<MenuItem> MenuItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderItem> OrderItems { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connection = "Data Source=SID\\SQLEXPRESS;Initial Catalog=OrderManagementDb; Integrated Security=SSPI;Trusted_Connection=True;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connection);
+        }
+    }
+}
